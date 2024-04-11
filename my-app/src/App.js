b8f19f6165bcdf785ec67292994fbd372fd3653d
Saki Chy <sakibchowdhury79@gmail.com>
2024-04-11 15:56:31 +0000

    add login form with event handlers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm.js"
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
       <Sidebar />
+      <NavBarForm />
+      
     </div>
   );
 }
